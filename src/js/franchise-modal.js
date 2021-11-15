e11a1b77181fc189d03f29166547f04feb2eebc4
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2021-11-15 20:48:16 +0200

    contacts/modal
    Підлючені файли для модельних вікон в index.html,  удалив в плейсхолдері анімацію кольорів (при ховері міняється тільки бекграунд)

---
+++ src/js/franchise-modal.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[franchise-open]'),
+    closeModalBtn: document.querySelector('[franchise-close]'),
+    modal: document.querySelector('[franchise-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();

fbe982243691ecae7bc2d8c4e7c00f28a59c0593
Olia Rosiak <olia.rosiak@gmail.com>
2021-11-17 11:37:47 +0200

    Костиль для моб.меню автомат.закриття при кліку

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,7 +1,13 @@
+// Костыль для мобильного меню, при клике закрывать само меню
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeLinkOne = document.querySelector('.js-link-one');
+  const closeLinkTwo = document.querySelector('.js-link-two');
+  const closeLinkThree = document.querySelector('.js-link-three');
+  const closeLinkFour = document.querySelector('.js-link-four');
+  
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +23,10 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeLinkOne.addEventListener('click', toggleMenu);
+  closeLinkTwo.addEventListener('click', toggleMenu);
+  closeLinkThree.addEventListener('click', toggleMenu);
+  closeLinkFour.addEventListener('click', toggleMenu);  
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
@@ -27,3 +37,34 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+// Вариант скрипта из дз
+// (() => {
+//   const mobileMenu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
+
+//   const toggleMenu = () => {
+//     const isMenuOpen =
+//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
+
+//     const scrollLockMethod = !isMenuOpen
+//       ? 'disableBodyScroll'
+//       : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   };
+
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Закрываем мобильное меню на более широких экранах
+//   // в случае изменения ориентации устройства.
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+// })();
